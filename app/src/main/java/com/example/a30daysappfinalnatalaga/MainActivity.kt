fdbe8ae12209db8c7d59774bae8ca489ec4c0ebd
vivisaur <ivyaquino981@gmail.com>
2024-10-25 14:09:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a30daysappfinalnatalaga/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.a30daysappfinalnatalaga
+
+import android.os.Bundle
+import android.widget.Switch
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val switchDarkMode: Switch = findViewById(R.id.switchDarkMode)
+
+        // Set default mode based on preferences
+        switchDarkMode.isChecked =
+            (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+
+        // Change mode based on switch state
+        switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
